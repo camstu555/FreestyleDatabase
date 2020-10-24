0612b80eed1fef0c406090482d42f693c94c557c
Cam <stew.cameron@gmail.com>
2020-10-24 05:44:37 -0400

    initial commit

---
+++ rankings-2/Models/WrestlingDataModel.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using rankings2.Services;
+
+namespace rankings2.Models
+{
+    public class WrestlingDataModel
+    {
+        public string Country1 { get; set; }
+
+        public string Country1Emoji
+        {
+            get
+            {
+               
+                try
+                {
+                    var code = CountryCodeConverter.ConvertThreeLetterNameToTwoLetterName(Country1);
+                    return string.Concat(code.Select(x => char.ConvertFromUtf32(x + 0x1F1A5)));
+                }
+                catch (Exception ex) {
+                    return ex.Message;
+                }
+            }
+        }
+
+        public string FullCountryName1
+        {
+            get
+            {
+                try
+                {
+                    var code = CountryCodeConverter.ConvertThreeLetterNameToTwoLetterName(Country1);
+                    return CountryCodeConverter.TwoLetterNameToDisplayName(code);
+                }
+                catch (Exception ex)
+                {
+                    return ex.Message;
+                }
+            }
+        }
+
+        public string FullCountryName2
+        {
+            get
+            {
+                try
+                {
+                    var code = CountryCodeConverter.ConvertThreeLetterNameToTwoLetterName(Country2);
+                    return CountryCodeConverter.TwoLetterNameToDisplayName(code);
+                }
+                catch (Exception ex)
+                {
+                    return ex.Message;
+                }
+            }
+        }
+
+        public string WeightClass { get; set; }
+
+        public string WrestlerName1 { get; set; }
+
+        public string Result { get; set; }
+
+        public string Score { get; set; }
+
+        public string Country2 { get; set; }
+
+        public string Country2Emoji
+        {
+            get
+            {
+                
+                try
+                {
+                    var code = CountryCodeConverter.ConvertThreeLetterNameToTwoLetterName(Country2);
+
+
+                    return string.Concat(code.Select(x => char.ConvertFromUtf32(x + 0x1F1A5)));
+                }
+                catch (Exception ex)
+                {
+                    return ex.Message;
+                }
+            }
+        }
+
+        public string WrestlerName2 { get; set; }
+
+        public string Result2 { get; set; }
+
+        public string Date { get; set; }
+
+        public string Venue { get; set; }
+
+        public string Round { get; set; }
+
+        public string Video { get; set; }
+
+    }
+}
