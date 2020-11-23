5b5ef2e5988d1a8a9a714cf16c5a7ad567a59a04
Cam <stew.cameron@gmail.com>
2020-11-23 10:25:40 -0500

    adds comments to show how to get values

--- FreestyleDatabase.Shared/Models/WrestlingAggregatesModel.cs
+++ FreestyleDatabase.Shared/Models/WrestlingAggregatesModel.cs
@@ -7,25 +7,30 @@ namespace FreestyleDatabase.Shared.Models
     public class WrestlingAggregatesModel
     {
         public int Wins { get; set; }
-
+        //count of WrestlerName1
         public int Losses { get; set; }
-
+        //count of WrestlerName2
         public List<string> GoldMedalMatches { get; set; }
-
+        //returns WrestlerName1 and Round = Gold
         public List<string> SilverMedalMatches { get; set; }
-
+        //returns WrestlerName2 and Round = Gold
         public List<string> BronzeMedalMatches { get; set; }
-
+        //returns WrestlerName1 and Round = Bronze
         public int Pins { get; set; }
-
+        //returns count of WrestlerName1 and result = VFA
         public int Techs { get; set; }
-
+        //returns count of WrestlerName1 and result = VSU or VSU1
         public int Points { get; set; }
-
+        //returns count of WrestlerName1 and result = VPO or VPO1
         public int AverageOffensivePointsPerMatch { get; set; }
-
+        //if (WrestlerName1 = search) add left of hyphen
+        //if (WrestlerName2 = search) add right of hyphen
+        //divide by count of total matches for that wrestler
         public int AverageDefensivePointsPerMatch { get; set; }
-
+        //if (WrestlerName1 = search) add right of hyphen
+        //if (WrestlerName2 = search) add left of hyphen
+        //divide by count of total matches for that wrestler
         public List<string> MostRecentMatches { get; set; }
+        //returns most recent 10 matches where WrestlerName1 or WrestlerName2 = search
     }
 }
