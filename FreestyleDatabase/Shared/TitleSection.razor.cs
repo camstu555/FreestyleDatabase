e1e57e4705f839fe77919c87401e16652f2b2725
Erik Zettersten <erik@zettersten.com>
2020-12-20 20:50:17 -0500

    ontact form

---
+++ FreestyleDatabase/Shared/TitleSection.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace FreestyleDatabase.Shared
+{
+    partial class TitleSection
+    {
+        [Parameter]
+        public string Title { get; set; } = "Page Name";
+
+        [Parameter]
+        public string SubTitle { get; set; } = "Your Page Sub Title";
+    }
+}
