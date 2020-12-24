ec93259bc6bf5cebf1246a76cac0212e3f6e424c
Cam <stew.cameron@gmail.com>
2020-12-23 21:05:34 -0500

    auto complete to search menu first attempt

--- FreestyleDatabase/Shared/SearchMenu.razor.cs
+++ FreestyleDatabase/Shared/SearchMenu.razor.cs
@@ -1,1 +1,61 @@
-﻿
+﻿using FreestyleDatabase.Shared.Models;
+using FreestyleDatabase.Shared.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FreestyleDatabase.Shared
+{
+    partial class SearchMenu
+    {
+
+        public WrestlingAutoCompleteModel selectedWrestler;
+
+        public WrestlingAggregatesModel result;
+
+        [Parameter]
+        public string Id { get; set; }
+
+        [Inject]
+        private WrestlerSearchService Http { get; set; }
+
+        [Inject]
+        private NavigationManager Nav { get; set; }
+
+        public void Dispose()
+        {
+            Nav.LocationChanged -= LocationChanged;
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            result = await Http.GetWrestlerDetails(Id);
+            Nav.LocationChanged += LocationChanged;
+        }
+
+        private async Task SelectedResultChanged(WrestlingAutoCompleteModel result)
+        {
+            var wrestler = await Http.GetWrestlerDetailsByName(result.Text);
+
+            Nav.NavigateTo("WrestlerProfile/" + wrestler.WrestlerId);
+        }
+
+        private void LocationChanged(object sender, LocationChangedEventArgs e)
+        {
+            InvokeAsync(async () =>
+            {
+                await Task.Delay(1);
+                result = await Http.GetWrestlerDetails(Id);
+                StateHasChanged();
+            });
+        }
+
+        private async Task<IEnumerable<WrestlingAutoCompleteModel>> SearchWrestlers(string searchText)
+        {
+            var response = await Http.GetAutoComplete(searchText);
+            return response.Items;
+        }
+
+    }
+}
