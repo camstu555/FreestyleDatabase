ca3e5aebd2f4077ddc08dbd10b025021a7f3bba1
Erik Zettersten <erik@zettersten.com>
2021-01-06 23:06:03 -0500

    fixes 'AutoComplete'

--- FreestyleDatabase.AzureFunction/FreestyleAutoComplete.cs
+++ FreestyleDatabase.AzureFunction/FreestyleAutoComplete.cs
@@ -1,8 +1,6 @@
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Threading.Tasks;
 
@@ -11,32 +9,28 @@ namespace FreestyleDatabase.AzureFunction
     public static class FreestyleAutoComplete
     {
         [FunctionName(nameof(FreestyleAutoComplete))]
-        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req, ILogger log)
+        public static async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequestData req)
         {
             try
             {
-                log.LogInformation("Attempting to autocomplete all wrestlers...");
+                Console.WriteLine("Attempting to autocomplete all wrestlers...");
 
-                var searchResults = await ServiceCollection.AzureSearchService.AutoComplete(req);
+                var url = req.GetDisplayUrl();
 
-                return new ContentResult
-                {
-                    Content = searchResults,
-                    ContentType = "application/json",
-                    StatusCode = 200
-                };
+                Console.WriteLine(url);
+
+                var searchResults = await ServiceCollection
+                    .AzureSearchService
+                    .AutoComplete(url);
+
+                return searchResults.ToResponse();
             }
-            catch (InvalidOperationException ex)
+            catch (Exception ex)
             {
-                log.LogInformation("An error occured.");
-                log.LogError(ex.Message);
+                Console.WriteLine("An error occured.");
+                Console.WriteLine(ex.ToString());
 
-                return new ContentResult
-                {
-                    Content = ex.Message,
-                    ContentType = "application/json",
-                    StatusCode = 400
-                };
+                return ex.ToResponse();
             }
         }
     }
